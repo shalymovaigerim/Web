f2a8a6979a9d1fad44007c8a36ca6087333a7812
Aigerim <shalymova_aigerim@mail.ru>
2020-12-04 02:02:54 +0500

    login,accounts

--- my_site/urls.py
+++ my_site/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name="index"),
-    path('about',views.about, name="about"),
-    path('contact',views.contact, name="contact")
+    path('',include('my_site.apps.public.urls')),
+    path('accounts/', include('my_site.apps.accounts.urls')),
+
 ]
