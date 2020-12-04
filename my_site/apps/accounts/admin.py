60395c21d0bee604cc6893c59ab2a54266536ff2
Aigerim <shalymova_aigerim@mail.ru>
2020-12-05 01:24:51 +0500

    bootstrap

--- my_site/apps/accounts/admin.py
+++ my_site/apps/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile, UserPersona, UserInterest
+
+admin.site.register(UserProfile)
+admin.site.register(UserPersona)
+admin.site.register(UserInterest)
+ 
