9d0cbcd9f306cbc9247f465ec4bf9daced053179
Aigerim <shalymova_aigerim@mail.ru>
2020-12-07 18:43:45 +0500

    vocabulary

--- my_site/apps/public/urls.py
+++ my_site/apps/public/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('',views.index, name="index"),
     path('about',views.about, name="about"),
     path('mission',views.mission, name="mission"),
+    path('grammar',views.grammar, name="grammar"),
+    path('vocabulary',views.vocabulary, name="vocabulary"),
+    path('food',views.food, name="food"),
+    path('animals',views.animals, name="animals"),
+    # path('sizes',views.sizes, name="sizes"),
+    # path('differentobjects',views.differentobjects, name="different objects"),
+    
 ]
--- my_site/apps/public/views.py
+++ my_site/apps/public/views.py
@@ -11,4 +11,21 @@ def about(request) :
 def mission(request):
     return render(request, 'mission.html')
 
+def grammar(request):
+    return render(request, "grammar.html")
+
+def vocabulary(request):
+    return render(request, "vocabualry.html")
+
+def food(request):
+    return render(request, "food.html")
+
+def animals(request):
+     return render(request, "animals.html")
+
+# def sizes(request):
+#     return render(request, "sizes.html")
+
+# def different objects(request):
+#     return render(request, "differentobjects.html")
 
